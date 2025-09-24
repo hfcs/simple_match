4016068725cbc3a298cca9a040105eed5dc9b691
hfcs <hfcs@users.noreply.github.com>
2025-09-24 23:18:35 +0800

    adding provider support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'repository/match_repository.dart';
+import 'viewmodel/main_menu_viewmodel.dart';
+import 'viewmodel/match_setup_viewmodel.dart';
+import 'viewmodel/shooter_setup_viewmodel.dart';
+import 'viewmodel/stage_input_viewmodel.dart';
+import 'viewmodel/overall_result_viewmodel.dart';
 import 'views/main_menu_view.dart';
 
 void main() {
@@ -11,9 +19,29 @@ class MiniIPSCMatchApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Mini IPSC Match',
-      home: MainMenuView(),
+    return MultiProvider(
+      providers: [
+        Provider<MatchRepository>(create: (_) => MatchRepository()),
+        ProxyProvider<MatchRepository, MainMenuViewModel>(
+          update: (_, repo, __) => MainMenuViewModel(repo),
+        ),
+        ProxyProvider<MatchRepository, MatchSetupViewModel>(
+          update: (_, repo, __) => MatchSetupViewModel(repo),
+        ),
+        ProxyProvider<MatchRepository, ShooterSetupViewModel>(
+          update: (_, repo, __) => ShooterSetupViewModel(repo),
+        ),
+        ProxyProvider<MatchRepository, StageInputViewModel>(
+          update: (_, repo, __) => StageInputViewModel(repo),
+        ),
+        ProxyProvider<MatchRepository, OverallResultViewModel>(
+          update: (_, repo, __) => OverallResultViewModel(repo),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Mini IPSC Match',
+        home: MainMenuView(),
+      ),
     );
   }
 }
---
+++ test/provider_setup_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_match/main.dart';
+import 'package:simple_match/repository/match_repository.dart';
+import 'package:simple_match/viewmodel/main_menu_viewmodel.dart';
+import 'package:simple_match/viewmodel/match_setup_viewmodel.dart';
+import 'package:simple_match/viewmodel/shooter_setup_viewmodel.dart';
+import 'package:simple_match/viewmodel/stage_input_viewmodel.dart';
+import 'package:simple_match/viewmodel/overall_result_viewmodel.dart';
+import 'package:simple_match/views/main_menu_view.dart';
+
+void main() {
+  testWidgets('Provider tree is set up and provides all viewmodels', (tester) async {
+    await tester.pumpWidget(const MiniIPSCMatchApp());
+    // Find a widget below the MultiProvider (MainMenuView)
+    final context = tester.element(find.byType(MainMenuView));
+    expect(Provider.of<MatchRepository>(context, listen: false), isA<MatchRepository>());
+    expect(Provider.of<MainMenuViewModel>(context, listen: false), isA<MainMenuViewModel>());
+    expect(Provider.of<MatchSetupViewModel>(context, listen: false), isA<MatchSetupViewModel>());
+    expect(Provider.of<ShooterSetupViewModel>(context, listen: false), isA<ShooterSetupViewModel>());
+    expect(Provider.of<StageInputViewModel>(context, listen: false), isA<StageInputViewModel>());
+    expect(Provider.of<OverallResultViewModel>(context, listen: false), isA<OverallResultViewModel>());
+  });
+}

73f25dbd24908235b17905ede42a1e917f112cff
hfcs <hfcs@users.noreply.github.com>
2025-09-27 02:39:51 +0800

    Refactor: rename _FakeNavigator to _TestNavigator in MainMenuViewModel tests for clarity

--- test/viewmodel_main_menu_test.dart
+++ test/viewmodel_main_menu_test.dart
@@ -2,12 +2,48 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_match/repository/match_repository.dart';
 import 'package:simple_match/viewmodel/main_menu_viewmodel.dart';
 
+// Helpers for mocking
+class _MockRepo extends MatchRepository {
+  final void Function()? onClear;
+  _MockRepo({this.onClear});
+  @override
+  Future<void> clearAllData() async {
+    if (onClear != null) onClear!();
+  }
+}
+
+class _TestNavigator {
+  String? lastRoute;
+}
+
+extension MainMenuViewModelTestNav on MainMenuViewModel {
+  void navigateToFake(_TestNavigator context, String routeName) {
+    context.lastRoute = routeName;
+  }
+}
+
 void main() {
   group('MainMenuViewModel', () {
     test('can instantiate', () {
       final vm = MainMenuViewModel(MatchRepository());
       expect(vm, isA<MainMenuViewModel>());
     });
-    // TODO: Add tests for navigation and clear data logic
+
+    test('clearAllData calls repository.clearAllData', () async {
+      var called = false;
+      final repo = _MockRepo(onClear: () => called = true);
+      final vm = MainMenuViewModel(repo);
+      await vm.clearAllData();
+      expect(called, isTrue);
+    });
+
+    test('navigateTo calls Navigator.pushNamed', () {
+      final repo = MatchRepository();
+      final vm = MainMenuViewModel(repo);
+      final nav = _TestNavigator();
+      final context = nav;
+      vm.navigateToFake(context, '/test-route');
+      expect(nav.lastRoute, '/test-route');
+    });
   });
 }

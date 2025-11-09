a95fc168fdf0e0fa5fcd4dd3a8dde46b5999d776
hfcs <hfcs@users.noreply.github.com>
2025-11-09 11:54:42 +0800

    Pin Gradle to OpenJDK 21 and target Java 21 for Android module

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,12 +11,13 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        // Use Java 21 as the target JVM for Kotlin
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     defaultConfig {
@@ -35,6 +36,15 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    // Prefer Gradle Java toolchain so Gradle can automatically provision/use a compatible JDK (Java 21)
+    // This requires a recent Gradle version that supports toolchains. If your Gradle doesn't support this,
+    // you can set org.gradle.java.home in android/gradle.properties to the JDK 21 installation path.
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
         }
     }
 }

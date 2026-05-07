f7b201dd2e7273461ca4e4fb82083aa7f55f9895
CI Trigger <devnull@example.com>
2026-05-08 00:52:56 +0800

    chore: remove tracked CI artifacts

--- ci-artifacts-23105757889/github-pages/lib/services/platform_info.dart
+++
@@ -1,1 +0,0 @@
-export 'platform_info_web.dart' if (dart.library.io) 'platform_info_io.dart';
--- ci-artifacts-23105757889/github-pages/test/widgets/settings_view_fill_uncovered_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('placeholder to avoid unstable tests', () {
-    expect(1, equals(1));
-  });
-}
